c45e6493482106177183863f66a06ee6af1eeaf5
Simon McEnlly <simon.mcenlly@gmail.com>
2021-06-22 08:20:51 +1000

    Remove unnecessary configuration file settings

--- wallaby.js
+++ wallaby.js
@@ -1,17 +1,3 @@
 module.exports = () => ({
   autoDetect: true,
-  files: [
-    '!**/packages/tests/integration/**/*',
-    { pattern: 'node_modules/@company/**', ignore: true }
-  ],
-  tests: [
-    '**/tests/unit/**/*.test.*',
-    { pattern: 'node_modules/@company/**', ignore: true }
-  ],
-  testFramework: {
-    // the jest configuration file path
-    // (relative to project root)
-    configFile: './jest.config.js'
-  },
-  debug: true
 });
